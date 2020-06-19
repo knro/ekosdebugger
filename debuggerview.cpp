ba3f9d44dcb719238c7cc76952eed2c0d7f47fae
Mohamad Kaouri <kaourimo@ikarustech.com>
2020-06-19 22:54:08 +0300

    Initial Skeleton

--- debuggerview.cpp
+++ debuggerview.cpp
@@ -6,10 +6,47 @@ DebuggerView::DebuggerView(QWidget *parent)
     , ui(new Ui::DebuggerView)
 {
     ui->setupUi(this);
+
+    connect(ui->startKStarsB, &QPushButton::clicked, this, &DebuggerView::startKStars);
+    connect(ui->stopKStarsB, &QPushButton::clicked, this, &DebuggerView::stopKStars);
 }
 
 DebuggerView::~DebuggerView()
 {
     delete ui;
+
+
 }
 
+void DebuggerView::startKStars()
+{
+    m_KStarsProcess = new QProcess();
+    QStringList args;
+    args << "-batch" << "-ex" << "run" << "kstars";
+
+    connect(m_KStarsProcess, &QProcess::readyReadStandardOutput, this, &DebuggerView::processKStarsOutput);
+    connect(m_KStarsProcess, &QProcess::readyReadStandardError, this, &DebuggerView::processKStarsError);
+    m_KStarsProcess->start("gdb", args);
+
+}
+
+void DebuggerView::stopKStars()
+{
+    m_KStarsProcess->terminate();
+
+
+}
+
+void DebuggerView::processKStarsOutput()
+{
+    QString buffer = m_KStarsProcess->readAllStandardOutput();
+    buffer = buffer.trimmed();
+    ui->KStarsDebugLog->append(buffer);
+}
+
+void DebuggerView::processKStarsError()
+{
+    QString buffer = m_KStarsProcess->readAllStandardError();
+    buffer = buffer.trimmed();
+    ui->KStarsAppLog->append(buffer);
+}
--- debuggerview.h
+++ debuggerview.h
@@ -2,20 +2,34 @@
 #define DEBUGGERVIEW_H
 
 #include <QMainWindow>
+#include <QProcess>
+#include <QPointer>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class DebuggerView; }
+namespace Ui
+{
+class DebuggerView;
+}
 QT_END_NAMESPACE
 
 class DebuggerView : public QMainWindow
 {
-    Q_OBJECT
+        Q_OBJECT
 
-public:
-    DebuggerView(QWidget *parent = nullptr);
-    ~DebuggerView();
+    public:
+        DebuggerView(QWidget *parent = nullptr);
+        ~DebuggerView();
 
-private:
-    Ui::DebuggerView *ui;
+    private slots:
+        void processKStarsOutput();
+        void processKStarsError();
+
+
+    private:
+        QPointer<QProcess> m_KStarsProcess;
+        Ui::DebuggerView *ui;
+        void startKStars();
+        void stopKStars();
+
 };
 #endif // DEBUGGERVIEW_H
